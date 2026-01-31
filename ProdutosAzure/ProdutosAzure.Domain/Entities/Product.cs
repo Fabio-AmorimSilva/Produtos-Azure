c3e30bc0012a3e790f82a1e259e6a4ce0dab7f8f
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2026-01-31 18:29:56 -0300

    feat: Creating Producy entity and ProductCategory enum

---
+++ ProdutosAzure/ProdutosAzure.Domain/Entities/Product.cs
@@ -0,0 +1,42 @@
+﻿namespace ProdutosAzure.Domain.Entities;
+
+public class Product
+{
+    public const int NameMaxLength = 150;
+    
+    public Guid Id { get; private set; }
+    public string Name { get; private set; }
+    public ProductCategory ProductCategory { get; private set; }
+    public DateTime CreatedAt { get; private set; }
+    public DateTime? UpdatedAt { get; private set; }
+
+    private Product()
+    {
+    }
+
+    public Product(
+        string name,
+        ProductCategory productCategory
+    )
+    {
+        Guard.IsNotNullOrWhiteSpace(name);
+        Guard.IsLessThanOrEqualTo(name.Length, NameMaxLength, nameof(name));
+        
+        Id = Guid.NewGuid();
+        Name = name;
+        ProductCategory = productCategory;
+        CreatedAt = DateTime.Now;
+    }
+
+    public void Update(
+        string name,
+        ProductCategory productCategory
+    )
+    {
+        Guard.IsNotNullOrWhiteSpace(name);
+        
+        Name = name;
+        ProductCategory = productCategory;
+        UpdatedAt = DateTime.Now;
+    }
+}
---
+++ ProdutosAzure/ProdutosAzure.Domain/Enums/ProductCategory.cs
@@ -0,0 +1,9 @@
+﻿namespace ProdutosAzure.Domain.Enums;
+
+public enum ProductCategory
+{
+    Clothes = 1,
+    Electronics = 2,
+    Food = 3,
+    Other = 4
+}
