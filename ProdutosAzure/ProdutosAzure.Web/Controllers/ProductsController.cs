2146dcee77a9694cee679a7ee8e483fceabe999a
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2026-01-31 18:47:53 -0300

    feat: Adding ProdutosAzure.Web project

---
+++ ProdutosAzure/ProdutosAzure.Web/Controllers/ProductsController.cs
@@ -0,0 +1,11 @@
+﻿namespace ProdutosAzure.Web.Controllers;
+
+public class ProductsController(IProductService productService) : Controller
+{
+    public async Task<IActionResult> Index()
+    {
+        var products = await productService.ListProductsAsync();
+        
+        return View(products);
+    }
+}
