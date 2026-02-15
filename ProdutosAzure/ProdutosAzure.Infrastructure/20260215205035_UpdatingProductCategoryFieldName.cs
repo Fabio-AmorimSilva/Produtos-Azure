e4b342da02eec4f4cdc30f25a2468fe4cba9d5c4
Fabio-AmorimSilva <fabioamorin120@gmail.com>
2026-02-15 17:56:23 -0300

    feat: Adding new migration

---
+++ ProdutosAzure/ProdutosAzure.Infrastructure/20260215205035_UpdatingProductCategoryFieldName.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProdutosAzure.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatingProductCategoryFieldName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ProductCategory",
+                table: "Products",
+                newName: "Category");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Category",
+                table: "Products",
+                newName: "ProductCategory");
+        }
+    }
+}
